b5487e8200cfdf9321e190eef850f0b95ff176a5
Bruno Henrique <brunohenrique.ramos@live.com>
2022-01-15 16:00:19 -0300

    feat: add colors variables

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,19 +6,59 @@ const breakpoints = [
 
 export type ThemeType = typeof theme
 
+const primaryColor = '#2484EA'
+const secondaryColor = '#2367AE'
+const tertiaryColor = '#ABCDEF'
+const quaternaryColor = '#3E4A72'
+
 const theme = {
   colors: {
-    black: '#222222',
-    primary: '#09A013',
-    secundary: '#AA8234',
-    terciary: '#ABCDEF',
-    text: '#222222',
-    white: '#FFFDF9'
+    primary: primaryColor,
+    secundary: secondaryColor,
+    tertiary: tertiaryColor,
+    quaternary: quaternaryColor,
+    background: {
+      primary: '#0E1122',
+      secondary: '#182037',
+      tertiary: '#131931',
+      quaternary: '#242D48'
+    },
+    button: {
+      success: {
+        darkGreen: '#1DAB91',
+        lightGreen: '#49CD57',
+        text: '#FFFFFF'
+      },
+      danger: {
+        darkRed: '#E21B25',
+        lightRed: '#E55C5C',
+        text: '#FFFFFF'
+      },
+      warning: {
+        darkOrange: '#ED8424',
+        lightOrange: '#FF9473',
+        text: '#FFFFFF'
+      },
+      default: {
+        primary: primaryColor,
+        secondary: secondaryColor,
+        text: '#FFFFFF'
+      }
+    },
+    text: {
+      light: '#9AA9D5',
+      dark: quaternaryColor
+    },
+    border: quaternaryColor,
+    icon: {
+      primary: primaryColor,
+      dark: quaternaryColor
+    }
   },
   fontSizes: {
-    large: '2.25rem',
+    small: '1.125rem',
     medium: '1.5rem',
-    small: '1.125rem'
+    large: '2.25rem'
   },
   breakpoints
 }
